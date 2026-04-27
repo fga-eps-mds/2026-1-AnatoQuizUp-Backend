41df876db525325b7236d3545014693b7c096b59
marialuisa214 <maria.luisa.alves214@gmail.com>
2026-04-27 01:26:08 -0300

    feat: Endpoint Altercao de status do usuario

--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -5,7 +5,11 @@ import { validarRequisicao } from "@/shared/middlewares/validacao.middleware";
 import { AdminService } from "./admin.service";
 import { AdminController } from "./admin.controller";
 import { UserRepository } from "./admin.repository";
-import { schemaBuscarUserPorId, schemaListarUsers } from "./admin.schemas";
+import {
+  schemaAlterarStatusUser,
+  schemaBuscarUserPorId,
+  schemaListarUsers,
+} from "./admin.schemas";
 
 const userRepository = new UserRepository();
 const adminService = new AdminService(userRepository);
@@ -18,5 +22,11 @@ adminRouter.get(
   "/users/:id",
   validarRequisicao(schemaBuscarUserPorId, "params"),
   adminController.buscarPorId,
+);
+adminRouter.patch(
+  "/users/:id/status",
+  validarRequisicao(schemaBuscarUserPorId, "params"),
+  validarRequisicao(schemaAlterarStatusUser),
+  adminController.alterarStatus,
 );
 export { adminRouter };
